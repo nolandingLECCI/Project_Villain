305eb2c1392b1bf568ae28686e26e9d78fb3b74f
ghdwl <ghdwls4654@naver.com>
2023-06-26 17:52:32 +0900

    메인메뉴, 메뉴바, 옵션 바 UI

--- Demo/Assets/Scripts/ScriptableObjects/CharacterBaseSO.cs
+++ Demo/Assets/Scripts/ScriptableObjects/CharacterBaseSO.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterBaseSO : MonoBehaviour
+public enum Rarity
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Common,
+    Rare,
+    Epic,
 }
+public enum Synergy
+{
+    None,
+    Ace,
+    Interest,
+    Newbie,
+    NightVil,
+    Saibi,
+    Vampire,
+}
+
+[CreateAssetMenu(fileName ="Assets/Resources/GameData/Characters/CharacterGameData", menuName = "vil/Character", order = 1)]
+    public class CharacterBaseSO : ScriptableObject
+    {
+        public int id;
+        public string Vil_Name;
+        public GameObject characterVisualsPrefab;
+        public Rarity rarity;
+        public Synergy Vil_Synergy_1;
+        public Synergy Vil_Synergy_2;
+
+        public int      Range_Normal;
+        public int      Range_Escape;
+        public float    Vil_Cooltime;
+        public int      Vil_Hp_Min;
+        public int      Vil_Str_Min;
+
+        public bool Vil_Demon;
+}   
+
