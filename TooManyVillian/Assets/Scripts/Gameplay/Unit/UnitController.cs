36b43cb552b725f9818ba99324d3ea265c12ad1b
ghdwl <ghdwls4654@naver.com>
2023-10-05 18:47:26 +0900

    대화창 UI, 일부 기능
    UI 배치 후 일부 기능 구현. 추후에 엑셀 대화 데이터와 연동

--- TooManyVillian/Assets/Scripts/Gameplay/Unit/UnitController.cs
+++ TooManyVillian/Assets/Scripts/Gameplay/Unit/UnitController.cs
@@ -1,227 +1,227 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UIToolkitDemo;
-using UnityEngine;
-using UnityEngine.UIElements;
-using Utilities.Inspector;
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UIToolkitDemo;
+// using UnityEngine;
+// using UnityEngine.UIElements;
+// using Utilities.Inspector;
 
-public class UnitController : MonoBehaviour
-{
-    [Header("Data")]
-    public UnitInfoData data;
+// public class UnitController : MonoBehaviour
+// {
+//     [Header("Data")]
+//     public UnitInfoData data;
 
-    [Header("Health Settings")]
-    public UnitHealthBehaviour healthBehaviour;
-    private bool unitIsAlive;
-    public HealthBarController healthBarBehaviour;
+//     [Header("Health Settings")]
+//     public UnitHealthBehaviour healthBehaviour;
+//     private bool unitIsAlive;
+//     public HealthBarController healthBarBehaviour;
 
-    [Header("Target Settings")]
-    public UnitTargetsBehaviour targetsBehaviour;
+//     [Header("Target Settings")]
+//     public UnitTargetsBehaviour targetsBehaviour;
 
-    [Header("Ability Settings")]
-    public UnitAbilitiesBehaviour abilitiesBehaviour;
+//     [Header("Ability Settings")]
+//     public UnitAbilitiesBehaviour abilitiesBehaviour;
 
-    [Header("Animation Settings")]
-    public UnitCharacterAnimationBehaviour characterAnimationBehaviour;
+//     [Header("Animation Settings")]
+//     public UnitCharacterAnimationBehaviour characterAnimationBehaviour;
 
-    [Header("Audio Settings")]
-    public UnitAudioBehaviour audioBehaviour;
+//     [Header("Audio Settings")]
+//     public UnitAudioBehaviour audioBehaviour;
 
-    [Header("UI")]
-    [SerializeField] private GameScreen m_GameScreen;
-    [SerializeField] private VisualTreeAsset m_CharacterVisualTree;
+//     [Header("UI")]
+//     [SerializeField] private GameScreen m_GameScreen;
+//     [SerializeField] private VisualTreeAsset m_CharacterVisualTree;
 
-    [Header("Debug")]
-    public bool initializeSelf;
+//     [Header("Debug")]
+//     public bool initializeSelf;
 
-    public delegate void UnitDiedEventHandler(UnitController unit);
-    public event UnitDiedEventHandler UnitDiedEvent;
+//     public delegate void UnitDiedEventHandler(UnitController unit);
+//     public event UnitDiedEventHandler UnitDiedEvent;
 
-    public static Action<UnitController> SpecialCharged;
-    public static Action<UnitController> SpecialDischarged;
-    public static Action<UnitController> UnitDied;
+//     public static Action<UnitController> SpecialCharged;
+//     public static Action<UnitController> SpecialDischarged;
+//     public static Action<UnitController> UnitDied;
 
-    private UnitAbilityBehaviour m_SpecialAbility;
-    private CharacterCard m_CharacterCard;
-    public CharacterCard CharacterCard => m_CharacterCard;
-    void Start()
-    {
-        if (initializeSelf)
-        {
-            SetAlive();
-            BattleStarted();
-        }
+//     private UnitAbilityBehaviour m_SpecialAbility;
+//     private CharacterCard m_CharacterCard;
+//     public CharacterCard CharacterCard => m_CharacterCard;
+//     void Start()
+//     {
+//         if (initializeSelf)
+//         {
+//             SetAlive();
+//             BattleStarted();
+//         }
 
-    }
+//     }
 
-    void OnEnable()
-    {
-        if (healthBehaviour != null)
-            healthBehaviour.HealthChanged += OnHealthChanged;
+//     void OnEnable()
+//     {
+//         if (healthBehaviour != null)
+//             healthBehaviour.HealthChanged += OnHealthChanged;
 
-        GameScreen.GamePaused += OnGamePaused;
-        GameScreen.GameResumed += OnGameResumed;
+//         GameScreen.GamePaused += OnGamePaused;
+//         GameScreen.GameResumed += OnGameResumed;
 
-        UnitAbilityBehaviour.AbilityCharged += OnAbilityCharged;
-        UnitAbilityBehaviour.AbilityDischarged += OnAbilityDischarged;
+//         UnitAbilityBehaviour.AbilityCharged += OnAbilityCharged;
+//         UnitAbilityBehaviour.AbilityDischarged += OnAbilityDischarged;
 
-    }
+//     }
 
-    void OnDisable()
-    {
-        if (healthBehaviour != null)
-            healthBehaviour.HealthChanged -= OnHealthChanged;
+//     void OnDisable()
+//     {
+//         if (healthBehaviour != null)
+//             healthBehaviour.HealthChanged -= OnHealthChanged;
 
-        GameScreen.GamePaused -= OnGamePaused;
-        GameScreen.GameResumed -= OnGameResumed;
+//         GameScreen.GamePaused -= OnGamePaused;
+//         GameScreen.GameResumed -= OnGameResumed;
 
-        UnitAbilityBehaviour.AbilityCharged += OnAbilityCharged;
-        UnitAbilityBehaviour.AbilityDischarged += OnAbilityDischarged;
-    }
+//         UnitAbilityBehaviour.AbilityCharged += OnAbilityCharged;
+//         UnitAbilityBehaviour.AbilityDischarged += OnAbilityDischarged;
+//     }
 
-    // event-handling methods
-    void OnGamePaused(float delay)
-    {
-        healthBarBehaviour?.DisplayHealthBar(false);
-    }
+//     // event-handling methods
+//     void OnGamePaused(float delay)
+//     {
+//         healthBarBehaviour?.DisplayHealthBar(false);
+//     }
 
-    void OnGameResumed()
-    {
+//     void OnGameResumed()
+//     {
 
-        healthBarBehaviour?.DisplayHealthBar(true);
-    }
+//         healthBarBehaviour?.DisplayHealthBar(true);
+//     }
 
-    void OnHealthChanged(int newCurrentHealth)
-    {
-        if (unitIsAlive)
-            healthBarBehaviour.UpdateHealth(newCurrentHealth);
-    }
+//     void OnHealthChanged(int newCurrentHealth)
+//     {
+//         if (unitIsAlive)
+//             healthBarBehaviour.UpdateHealth(newCurrentHealth);
+//     }
 
-    // notify GameScreen to enable FrameFX
-    void OnAbilityCharged(UnitAbilityBehaviour ability)
-    {
-        if (ability == m_SpecialAbility)
-        {
-            //Debug.Log(ability.data.abilityName + " charged on unit " + this.data.unitName);
-            SpecialCharged?.Invoke(this);
-        }
-    }
+//     // notify GameScreen to enable FrameFX
+//     void OnAbilityCharged(UnitAbilityBehaviour ability)
+//     {
+//         if (ability == m_SpecialAbility)
+//         {
+//             //Debug.Log(ability.data.abilityName + " charged on unit " + this.data.unitName);
+//             SpecialCharged?.Invoke(this);
+//         }
+//     }
 
-    // notify GameScreen to disable FrameFX
-    void OnAbilityDischarged(UnitAbilityBehaviour ability)
-    {
-        if (ability == m_SpecialAbility)
-        {
-            //Debug.Log(ability.data.abilityName + " discharged on unit " + this.data.unitName);
-            SpecialDischarged?.Invoke(this);
-        }
-    }
-
-
-
-    public void AssignTargetUnits(List<UnitController> units)
-    {
-        targetsBehaviour.AddTargetUnits(units);
-    }
-
-    public void RemoveTargetUnit(UnitController unit)
-    {
-        targetsBehaviour.RemoveTargetUnit(unit);
-    }
-
-    public void SetAlive()
-    {
-        healthBehaviour?.SetupCurrentHealth(data.totalHealth);
-        healthBarBehaviour?.DisplayHealthBar(true);
-        healthBarBehaviour?.SetHealth(data.totalHealth, data.totalHealth);
-
-        unitIsAlive = true;
+//     // notify GameScreen to disable FrameFX
+//     void OnAbilityDischarged(UnitAbilityBehaviour ability)
+//     {
+//         if (ability == m_SpecialAbility)
+//         {
+//             //Debug.Log(ability.data.abilityName + " discharged on unit " + this.data.unitName);
+//             SpecialDischarged?.Invoke(this);
+//         }
+//     }
 
 
-        if (m_GameScreen != null && m_CharacterVisualTree != null)
-        {
-            m_CharacterCard = gameObject.AddComponent<CharacterCard>();
-            m_CharacterCard.CharacterVisualTreeAsset = m_CharacterVisualTree;
-            m_CharacterCard.HeroData = data;
-            m_GameScreen.AddHero(m_CharacterCard);
 
-            // designate the last Ability as the "special" -- for the UI counter
-            if (abilitiesBehaviour != null)
-            {
-                m_SpecialAbility = abilitiesBehaviour.abilities[abilitiesBehaviour.abilities.Length - 1];
-                //Debug.Log("Special Ability for " + data.unitName + " is " + m_SpecialAbility.data.abilityName);
-            }
+//     public void AssignTargetUnits(List<UnitController> units)
+//     {
+//         targetsBehaviour.AddTargetUnits(units);
+//     }
 
-        }
-    }
+//     public void RemoveTargetUnit(UnitController unit)
+//     {
+//         targetsBehaviour.RemoveTargetUnit(unit);
+//     }
 
-    public void BattleStarted()
-    {
-        if (m_GameScreen != null && m_CharacterVisualTree != null)
-        {
-            abilitiesBehaviour.StartAbilityCooldowns(m_CharacterCard.CooldownBar);
-        }
-        else
-        {
-            abilitiesBehaviour.StartAbilityCooldowns();
-        }
-    }
+//     public void SetAlive()
+//     {
+//         healthBehaviour?.SetupCurrentHealth(data.totalHealth);
+//         healthBarBehaviour?.DisplayHealthBar(true);
+//         healthBarBehaviour?.SetHealth(data.totalHealth, data.totalHealth);
 
-    public void BattleEnded()
-    {
-        abilitiesBehaviour.StopAllAbilities();
-    }
+//         unitIsAlive = true;
 
-    public void AbilityHappened(int abilityValue, TargetType unitTargetType)
-    {
-        List<UnitController> targetUnits = targetsBehaviour.FilterTargetUnits(unitTargetType);
 
-        if (targetUnits.Count > 0)
-        {
-            for (int i = 0; i < targetUnits.Count; i++)
-            {
-                targetUnits[i].ReceiveAbilityValue(abilityValue);
-            }
-        }
-    }
+//         if (m_GameScreen != null && m_CharacterVisualTree != null)
+//         {
+//             m_CharacterCard = gameObject.AddComponent<CharacterCard>();
+//             m_CharacterCard.CharacterVisualTreeAsset = m_CharacterVisualTree;
+//             m_CharacterCard.HeroData = data;
+//             m_GameScreen.AddHero(m_CharacterCard);
 
-    public void ReceiveAbilityValue(int abilityValue)
-    {
-        if (unitIsAlive)
-        {
-            healthBehaviour.ChangeHealth(abilityValue);
-            characterAnimationBehaviour.CharacterWasHit();
+//             // designate the last Ability as the "special" -- for the UI counter
+//             if (abilitiesBehaviour != null)
+//             {
+//                 m_SpecialAbility = abilitiesBehaviour.abilities[abilitiesBehaviour.abilities.Length - 1];
+//                 //Debug.Log("Special Ability for " + data.unitName + " is " + m_SpecialAbility.data.abilityName);
+//             }
 
-            healthBarBehaviour.UpdateHealth(GetCurrentHealth());
-        }
-    }
+//         }
+//     }
 
-    public int GetCurrentHealth()
-    {
-        return healthBehaviour.GetCurrentHealth();
-    }
+//     public void BattleStarted()
+//     {
+//         if (m_GameScreen != null && m_CharacterVisualTree != null)
+//         {
+//             abilitiesBehaviour.StartAbilityCooldowns(m_CharacterCard.CooldownBar);
+//         }
+//         else
+//         {
+//             abilitiesBehaviour.StartAbilityCooldowns();
+//         }
+//     }
 
-    public void UnitHasDied()
-    {
-        unitIsAlive = false;
+//     public void BattleEnded()
+//     {
+//         abilitiesBehaviour.StopAllAbilities();
+//     }
 
-        healthBarBehaviour?.DisplayHealthBar(false);
+//     public void AbilityHappened(int abilityValue, TargetType unitTargetType)
+//     {
+//         List<UnitController> targetUnits = targetsBehaviour.FilterTargetUnits(unitTargetType);
 
-        abilitiesBehaviour.StopAllAbilities();
-        characterAnimationBehaviour.CharacterHasDied();
+//         if (targetUnits.Count > 0)
+//         {
+//             for (int i = 0; i < targetUnits.Count; i++)
+//             {
+//                 targetUnits[i].ReceiveAbilityValue(abilityValue);
+//             }
+//         }
+//     }
 
-        UnitDied?.Invoke(this);
+//     public void ReceiveAbilityValue(int abilityValue)
+//     {
+//         if (unitIsAlive)
+//         {
+//             healthBehaviour.ChangeHealth(abilityValue);
+//             characterAnimationBehaviour.CharacterWasHit();
 
-        // notify BattleGameManager
-        DelegateEventUnitDied();
-    }
+//             healthBarBehaviour.UpdateHealth(GetCurrentHealth());
+//         }
+//     }
 
-    void DelegateEventUnitDied()
-    {
-        if (UnitDiedEvent != null)
-        {
-            UnitDiedEvent(this);
-        }
-    }
-}
+//     public int GetCurrentHealth()
+//     {
+//         return healthBehaviour.GetCurrentHealth();
+//     }
+
+//     public void UnitHasDied()
+//     {
+//         unitIsAlive = false;
+
+//         healthBarBehaviour?.DisplayHealthBar(false);
+
+//         abilitiesBehaviour.StopAllAbilities();
+//         characterAnimationBehaviour.CharacterHasDied();
+
+//         UnitDied?.Invoke(this);
+
+//         // notify BattleGameManager
+//         DelegateEventUnitDied();
+//     }
+
+//     void DelegateEventUnitDied()
+//     {
+//         if (UnitDiedEvent != null)
+//         {
+//             UnitDiedEvent(this);
+//         }
+//     }
+// }
---
+++ TooManyVillian/Assets/Scripts/UI/DialogScreen/DialogScreen.cs
@@ -0,0 +1,206 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+using UnityEngine.UIElements;
+
+public class DialogScreen : MonoBehaviour
+{
+    //Screen Settings
+    [SerializeField] protected UIDocument m_Document;
+    protected VisualElement m_Screen;
+    protected VisualElement m_Root;
+
+    public event Action ScreenStarted;
+    public event Action ScreenEnded;
+
+    //Dialog System
+    private bool isAutoStart = false;
+    private bool isFirst = true;
+    private bool playOnlyFirstTime = true;
+    private int currentDialogIndex = -1;
+    private int currentSpeakerIndex = 0;
+    private float defaultTypingSpeed = 0.1f;
+    private float ffTypingSpeed = 0.05f;
+    private bool fastForward = false;
+
+    private bool isTypingEffect = false;
+
+
+    //UI Settings
+    const string k_ClickButton = "Dialog_Click-button";
+
+    const string k_SkipButton = "Dialog_Menu-skip_button";
+    const string k_FFButton = "Dialog_Menu-ff_button";
+    const string k_AutoButton = "Dialog_Menu-auto_button";
+
+    const string k_CenterImage = "Dialog_Image-center";
+    const string k_LeftImage = "Dialog_Image-left";
+    const string k_RightImage = "Dialog_Image-right";
+
+    const string k_Name = "Dialog_Text-name";
+    const string k_Text = "Dialog_Text-text";
+
+    const string k_Next = "Dialog_Text-next";
+
+    Button m_ClickButton;
+
+    Button m_SkipButton;
+    Button m_FFButton;
+    Button m_AutoButton;
+
+    VisualElement m_CenterImage;
+    VisualElement m_RightImage;
+    VisualElement m_LeftImage;
+
+    Label m_Name;
+    Label m_Text;
+
+    VisualElement m_Next;
+
+    protected virtual void Awake()
+    {
+        if (m_Document == null)
+            m_Document = GetComponent<UIDocument>();
+        else
+        {
+            SetVisualElements();
+            RegisterButtonCallbacks();
+        }
+        Setup();
+    }
+
+    void OnEnable()
+    {
+        ShowScreen();
+    }
+
+    private void SetVisualElements()
+    {
+        if (m_Document != null)
+                m_Root = m_Document.rootVisualElement;
+        m_Screen = m_Root.Q("DialogScreen");
+
+        m_ClickButton = m_Root.Q<Button>(k_ClickButton);
+
+        m_SkipButton = m_Root.Q<Button>(k_SkipButton);
+        m_FFButton = m_Root.Q<Button>(k_FFButton);
+        m_AutoButton = m_Root.Q<Button>(k_AutoButton);
+
+        m_CenterImage = m_Root.Q<VisualElement>(k_CenterImage);
+        m_LeftImage = m_Root.Q<VisualElement>(k_LeftImage);
+        m_RightImage = m_Root.Q<VisualElement>(k_RightImage);
+
+        m_Name= m_Root.Q<Label>(k_Name);
+        m_Text= m_Root.Q<Label>(k_Text);
+
+        m_Next = m_Root.Q<VisualElement>(k_Next);
+    }
+    private void RegisterButtonCallbacks()
+    {
+        m_ClickButton?.RegisterCallback<ClickEvent>(ScreenClicked);
+
+        m_SkipButton?.RegisterCallback<ClickEvent>(Skip);
+        m_FFButton?.RegisterCallback<ClickEvent>(FF);
+        m_AutoButton?.RegisterCallback<ClickEvent>(Auto);
+    }
+
+    void ScreenClicked(ClickEvent evt)
+    {
+        //m_Text.text = "test test test test test test test test test test test test test test test test test test test test test test test test ";
+    }
+    void Skip(ClickEvent evt)
+    {
+        
+    }
+    void FF(ClickEvent evt)
+    {
+        if(!fastForward)    fastForward = true;
+        else                fastForward = false;
+    }
+    void Auto(ClickEvent evt)
+    {
+        if(!isAutoStart)    isAutoStart = true;
+        else                isAutoStart = false;
+    }
+    
+
+    public bool IsVisible()
+    {
+        if (m_Screen == null)
+            return false;
+
+        return (m_Screen.style.display == DisplayStyle.Flex);
+    }
+
+    
+    public static void ShowVisualElement(VisualElement visualElement, bool state)
+    {
+        if (visualElement == null)
+            return;
+
+        visualElement.style.display = (state) ? DisplayStyle.Flex : DisplayStyle.None;
+    }
+
+    
+    public VisualElement GetVisualElement(string elementName)
+    {
+        if (string.IsNullOrEmpty(elementName) || m_Root == null)
+            return null;
+
+        
+        return m_Root.Q(elementName);
+    }
+
+    public virtual void ShowScreen()
+    {
+        ShowVisualElement(m_Screen, true);
+        ScreenStarted?.Invoke();
+    }
+
+    public virtual void HideScreen()
+    {
+        if (IsVisible())
+        {
+            ShowVisualElement(m_Screen, false);
+            ScreenEnded?.Invoke();
+        }
+    }
+
+    private void Setup()
+    {
+
+    }
+
+    // public bool UpdateDialog()
+    // {
+        
+    // }
+    // private void SetNextDialog()
+    // {
+
+    // }
+    private IEnumerator OnTypingText()
+    {
+        int idx = 0;
+        float typingSpeed = defaultTypingSpeed;
+
+        isTypingEffect = true;
+
+        while(idx < m_Text.text.Length)
+        {
+            m_Text.text = m_Text.text.Substring(0,idx);
+            idx++;
+
+            if(!fastForward)    typingSpeed = defaultTypingSpeed;
+            else                typingSpeed = ffTypingSpeed;
+
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        isTypingEffect = false;
+
+        //speakers[currentSpeakerIndex].objectArrow.SetActive(true);
+    }
+}
